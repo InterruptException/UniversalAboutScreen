bc336b66180afa8ba235efb407c2113f17ee390a
chenqiangsheng <cqs4work_zhy@outlook.com>
2024-01-09 16:14:18 +0800

    增加权限和加载网页

--- aboutscreen/src/main/java/com/runfeng1688/lib/aboutscreen/AppPrivacyScreenActivity.java
+++ aboutscreen/src/main/java/com/runfeng1688/lib/aboutscreen/AppPrivacyScreenActivity.java
@@ -3,6 +3,10 @@ package com.runfeng1688.lib.aboutscreen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class AppPrivacyScreenActivity extends AppCompatActivity {
 
@@ -11,5 +15,18 @@ public class AppPrivacyScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_privacy_screen);
 //        getSupportActionBar()
+
+        MaterialToolbar toolbar = findViewById(R.id.topAppBar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        String link = getIntent().getStringExtra("LINK");
+        assert link != null;
+        WebView wv = findViewById(R.id.wv);
+        wv.loadUrl(link);
+
     }
 }
